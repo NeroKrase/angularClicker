835104ab46012b3d3b04696057c2a98cdf27d5d9
Nero Krase <pavelhbt@gmail.com>
2020-12-09 17:55:30 +0200

    deploy commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StartMenuComponent} from "./start-menu/start-menu.component";
+import {PlayerProfileComponent} from "./player-profile/player-profile.component";
+import {ClickerGameComponent} from "./clicker-game/clicker-game.component";
+import {LeaderboardComponent} from "./leaderboard/leaderboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'start-menu',
+    component: StartMenuComponent
+  },
+  {
+    path: 'player-profile/:name',
+    component: PlayerProfileComponent
+  },
+  {
+    path: 'records/:time',
+    component: LeaderboardComponent
+  },
+  {
+    path: 'game/:time',
+    component: ClickerGameComponent
+  },
+  {
+    path: '',
+    redirectTo: '/records/5',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartMenuComponent } from './start-menu/start-menu.component';
+import {PlayersService} from "./players.service";
+import {FormsModule} from "@angular/forms";
+import { PlayerProfileComponent } from './player-profile/player-profile.component';
+import { ButtonComponent } from './button/button.component';
+import { ClickerGameComponent } from './clicker-game/clicker-game.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartMenuComponent,
+    PlayerProfileComponent,
+    ButtonComponent,
+    ClickerGameComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PlayersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
